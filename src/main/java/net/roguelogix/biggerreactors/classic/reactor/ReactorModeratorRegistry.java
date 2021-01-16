1a27b03e4b5a8153842cf2b6cc7d33b7361fd522
RogueLogix <roguelogix@roguelogix.net>
2021-01-15 21:47:38 -0800

    move registries over to Phosphophyllite data loader

--- src/main/java/net/roguelogix/biggerreactors/classic/reactor/ReactorModeratorRegistry.java
+++ src/main/java/net/roguelogix/biggerreactors/classic/reactor/ReactorModeratorRegistry.java
@@ -9,16 +9,15 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.ResourceLocationException;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.roguelogix.biggerreactors.BiggerReactors;
+import net.roguelogix.biggerreactors.Config;
+import net.roguelogix.phosphophyllite.data.DataLoader;
 import net.roguelogix.phosphophyllite.repack.tnjson.ParseException;
 import net.roguelogix.phosphophyllite.repack.tnjson.TnJson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ReactorModeratorRegistry {
     
@@ -39,7 +38,7 @@ public class ReactorModeratorRegistry {
     }
     
     private final static HashMap<Block, ModeratorProperties> registry = new HashMap<>();
-
+    
     public static Map<Block, ModeratorProperties> getImmutableRegistry() {
         return Collections.unmodifiableMap(registry);
     }
@@ -52,162 +51,42 @@ public class ReactorModeratorRegistry {
         return registry.get(block);
     }
     
+    private static class ReactorModeratorJsonData {
+        
+        @DataLoader.Values({"tag", "registry"})
+        String type;
+        
+        ResourceLocation location;
+        
+        @DataLoader.Range("[0,1]")
+        double absorption;
+        
+        @DataLoader.Range("[0,1]")
+        double efficiency;
+        
+        @DataLoader.Range("[1,)")
+        double moderation;
+        
+        @DataLoader.Range("[0,)")
+        double conductivity;
+    }
+    
+    private static final DataLoader<ReactorModeratorJsonData> dataLoader = new DataLoader<>(ReactorModeratorJsonData.class);
+    
     public static void loadRegistry(ITagCollection<Block> blockTags) {
+        
         BiggerReactors.LOGGER.info("Loading reactor moderators");
         registry.clear();
-        // TODO: generify this code in Phosphophyllite
-        IResourceManager resourceManager = BiggerReactors.dataPackRegistries.getResourceManager();
-        Collection<ResourceLocation> resourceLocations = resourceManager.getAllResourceLocations("ebcr/moderators", s -> s.contains(".json"));
         
-        for (ResourceLocation resourceLocation : resourceLocations) {
-            if (!resourceLocation.getNamespace().equals("biggerreactors")) {
-                continue;
-            }
-            String json;
-            try {
-                IResource resource = resourceManager.getResource(resourceLocation);
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
-                    StringBuilder builder = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        builder.append(line);
-                    }
-                    json = builder.toString();
-                }
-                
-            } catch (IOException e) {
-                BiggerReactors.LOGGER.error("Error reading moderator json at " + resourceLocation.toString());
-                e.printStackTrace();
-                continue;
-            }
+        List<ReactorModeratorJsonData> data = dataLoader.loadAll(new ResourceLocation("biggerreactors:ebcr/moderators"));
+        BiggerReactors.LOGGER.info("Loaded " + data.size() + " moderator data entries");
+        
+        for (ReactorModeratorJsonData moderatorData : data) {
             
-            Map<String, Object> map;
-            try {
-                map = TnJson.parse(json);
-            } catch (ParseException e) {
-                BiggerReactors.LOGGER.error("Error parsing moderator json at " + resourceLocation.toString());
-                e.printStackTrace();
-                continue;
-            }
+            ModeratorProperties properties = new ModeratorProperties(moderatorData.absorption, moderatorData.efficiency, moderatorData.moderation, moderatorData.conductivity);
             
-            Object typeObject = map.get("type");
-            Object locationObject = map.get("location");
-            Object absorptionObject = map.get("absorption");
-            Object efficiencyObject = map.get("efficiency");
-            Object moderationObject = map.get("moderation");
-            Object conductivityObject = map.get("conductivity");
-            
-            if (typeObject == null) {
-                BiggerReactors.LOGGER.error("Error moderator location type not found in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (locationObject == null) {
-                BiggerReactors.LOGGER.error("Error moderator location not found in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (absorptionObject == null) {
-                BiggerReactors.LOGGER.error("Error moderator absorption not found in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (efficiencyObject == null) {
-                BiggerReactors.LOGGER.error("Error moderator efficiency not found in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (moderationObject == null) {
-                BiggerReactors.LOGGER.error("Error moderator moderation not found in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (conductivityObject == null) {
-                BiggerReactors.LOGGER.error("Error moderator conductivity not found in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (!(typeObject instanceof String)) {
-                BiggerReactors.LOGGER.error("Error moderator location type not a string in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (!(locationObject instanceof String)) {
-                BiggerReactors.LOGGER.error("Error moderator location not a string in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (!(absorptionObject instanceof Number)) {
-                BiggerReactors.LOGGER.error("Error moderator absorption not a number in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (!(efficiencyObject instanceof Number)) {
-                BiggerReactors.LOGGER.error("Error moderator efficiency not a number in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (!(moderationObject instanceof Number)) {
-                BiggerReactors.LOGGER.error("Error moderator moderation not a number in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            if (!(conductivityObject instanceof Number)) {
-                BiggerReactors.LOGGER.error("Error moderator conductivity not a number in json at " + resourceLocation.toString());
-                continue;
-            }
-            
-            String typeStr = (String) typeObject;
-            String locationStr = (String) locationObject;
-            double absorption = ((Number) absorptionObject).doubleValue();
-            double efficiency = ((Number) efficiencyObject).doubleValue();
-            double moderation = ((Number) moderationObject).doubleValue();
-            double conductivity = ((Number) conductivityObject).doubleValue();
-            
-            boolean isTag;
-            if (typeStr.equals("tag")) {
-                isTag = true;
-            } else if (typeStr.equals("registry")) {
-                isTag = false;
-            } else {
-                BiggerReactors.LOGGER.error("Error location type not a \"tag\" or \"registry\" in json at " + resourceLocation.toString() + ", got " + typeStr);
-                continue;
-            }
-            
-            ResourceLocation location;
-            try {
-                location = new ResourceLocation(locationStr);
-            } catch (ResourceLocationException e) {
-                BiggerReactors.LOGGER.error("Error invalid resource location given in json at " + resourceLocation.toString() + ", got " + locationStr);
-                e.printStackTrace();
-                continue;
-            }
-            
-            
-            if (absorption < 0.0 || absorption > 1.0) {
-                BiggerReactors.LOGGER.error("Error absorption out of range in json at " + resourceLocation.toString() + ", got " + absorption + ", valid range is [0, 1], clamping");
-                absorption = Math.max(0.0, Math.min(1.0, absorption));
-            }
-            
-            if (efficiency < 0.0 || efficiency > 1.0) {
-                BiggerReactors.LOGGER.error("Error efficiency out of range in json at " + resourceLocation.toString() + ", got " + efficiency + ", valid range is [0, 1], clamping");
-                efficiency = Math.max(0.0, Math.min(1.0, efficiency));
-            }
-            
-            if (moderation < 1.0) {
-                BiggerReactors.LOGGER.error("Error moderation out of range in json at " + resourceLocation.toString() + ", got " + moderation + ", valid range is [1,), clamping");
-                moderation = Math.max(1.0, moderation);
-            }
-            
-            if (conductivity < 0.0) {
-                BiggerReactors.LOGGER.error("Error conductivity out of range in json at " + resourceLocation.toString() + ", got " + conductivity + ", valid range is [0,), clamping");
-                conductivity = Math.max(0.0, conductivity);
-            }
-            
-            ModeratorProperties properties = new ModeratorProperties(absorption, efficiency, moderation, conductivity);
-            
-            if (isTag) {
-                ITag<Block> blockTag = blockTags.get(location);
+            if (moderatorData.type.equals("tag")) {
+                ITag<Block> blockTag = blockTags.get(moderatorData.location);
                 if (blockTag == null) {
                     continue;
                 }
@@ -217,9 +96,9 @@ public class ReactorModeratorRegistry {
                 }
             } else {
                 // cant check against air, because air is a valid thing to load
-                if(ForgeRegistries.BLOCKS.containsKey(location)) {
-                    registry.put(ForgeRegistries.BLOCKS.getValue(location), properties);
-                    BiggerReactors.LOGGER.debug("Loaded moderator " + location);
+                if (ForgeRegistries.BLOCKS.containsKey(moderatorData.location)) {
+                    registry.put(ForgeRegistries.BLOCKS.getValue(moderatorData.location), properties);
+                    BiggerReactors.LOGGER.debug("Loaded moderator " + moderatorData.location);
                 }
             }
         }
