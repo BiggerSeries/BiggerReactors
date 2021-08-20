c4eee0cd97c834adf84552c3432fd86d88daf0f5
RogueLogix <roguelogix@roguelogix.net>
2021-08-20 00:27:41 -0700

    re-enable computercraft integration
    CC:T is on 1.17 now, so, integration is back

--- src/main/java/net/roguelogix/biggerreactors/multiblocks/heatexchanger/tiles/HeatExchangerComputerPortTile.java
+++ src/main/java/net/roguelogix/biggerreactors/multiblocks/heatexchanger/tiles/HeatExchangerComputerPortTile.java
@@ -1,12 +1,15 @@
 package net.roguelogix.biggerreactors.multiblocks.heatexchanger.tiles;
 
+import dan200.computercraft.api.peripheral.IPeripheral;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.util.LazyOptional;
+import net.roguelogix.biggerreactors.multiblocks.heatexchanger.deps.HeatExchangerPeripheral;
 import net.roguelogix.phosphophyllite.registry.RegisterTileEntity;
 
 import javax.annotation.Nullable;
@@ -27,14 +30,14 @@ public class HeatExchangerComputerPortTile extends HeatExchangerBaseTile {
         super(TYPE, pos, state);
     }
 
-//    @CapabilityInject(IPeripheral.class)
-//    public static Capability<IPeripheral> CAPABILITY_PERIPHERAL = null;
+    @CapabilityInject(IPeripheral.class)
+    public static Capability<IPeripheral> CAPABILITY_PERIPHERAL = null;
     
     @Override
     public <T> LazyOptional<T> capability(Capability<T> cap, final @Nullable Direction side) {
-//        if (cap == CAPABILITY_PERIPHERAL) {
-//            return HeatExchangerPeripheral.create(() -> controller).cast();
-//        }
+        if (cap == CAPABILITY_PERIPHERAL) {
+            return HeatExchangerPeripheral.create(this::controller).cast();
+        }
         return LazyOptional.empty();
     }
     
--- src/main/java/net/roguelogix/biggerreactors/multiblocks/reactor/tiles/ReactorComputerPortTile.java
+++ src/main/java/net/roguelogix/biggerreactors/multiblocks/reactor/tiles/ReactorComputerPortTile.java
@@ -1,12 +1,15 @@
 package net.roguelogix.biggerreactors.multiblocks.reactor.tiles;
 
+import dan200.computercraft.api.peripheral.IPeripheral;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.util.LazyOptional;
+import net.roguelogix.biggerreactors.multiblocks.reactor.deps.ReactorPeripheral;
 import net.roguelogix.phosphophyllite.multiblock.IOnAssemblyTile;
 import net.roguelogix.phosphophyllite.registry.RegisterTileEntity;
 
@@ -28,31 +31,31 @@ public class ReactorComputerPortTile extends ReactorBaseTile implements IOnAssem
         super(TYPE, pos, state);
     }
 
-//    @CapabilityInject(IPeripheral.class)
-//    public static Capability<IPeripheral> CAPABILITY_PERIPHERAL = null;
-//
-//    private LazyOptional<ReactorPeripheral> peripheralCapability;
-//
-//    {
-////         avoids classloading without CC existing
-//        if (CAPABILITY_PERIPHERAL != null) {
-//            peripheralCapability = ReactorPeripheral.create(() -> controller);
-//        }
-//    }
+    @CapabilityInject(IPeripheral.class)
+    public static Capability<IPeripheral> CAPABILITY_PERIPHERAL = null;
+
+    private LazyOptional<ReactorPeripheral> peripheralCapability;
+
+    {
+        // avoids classloading without CC existing
+        if (CAPABILITY_PERIPHERAL != null) {
+            peripheralCapability = ReactorPeripheral.create(this::controller);
+        }
+    }
     
     @Override
     public <T> LazyOptional<T> capability(Capability<T> cap, final @Nullable Direction side) {
-//        if (cap == CAPABILITY_PERIPHERAL) {
-//            return peripheralCapability.cast();
-//        }
+        if (cap == CAPABILITY_PERIPHERAL) {
+            return peripheralCapability.cast();
+        }
         return LazyOptional.empty();
     }
     
     @Override
     public void onAssembly() {
         // class loading BS, dont remove this if
-//        if (CAPABILITY_PERIPHERAL != null) {
-//            peripheralCapability.ifPresent(ReactorPeripheral::rebuildControlRodList);
-//        }
+        if (CAPABILITY_PERIPHERAL != null) {
+            peripheralCapability.ifPresent(ReactorPeripheral::rebuildControlRodList);
+        }
     }
 }
