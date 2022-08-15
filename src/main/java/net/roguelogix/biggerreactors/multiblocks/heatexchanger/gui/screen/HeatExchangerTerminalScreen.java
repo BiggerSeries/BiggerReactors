2ec291a11751200a5584862c36075100cc9ec1e2
Eric <gizmocodes@users.noreply.github.com>
2022-08-14 22:33:32 -0500

    Fix GUI labels no longer being centered

--- src/main/java/net/roguelogix/biggerreactors/multiblocks/heatexchanger/gui/screen/HeatExchangerTerminalScreen.java
+++ src/main/java/net/roguelogix/biggerreactors/multiblocks/heatexchanger/gui/screen/HeatExchangerTerminalScreen.java
@@ -55,7 +55,7 @@ public class HeatExchangerTerminalScreen extends PhosphophylliteScreen<HeatExcha
         super.init();
 
         // Set title to be drawn in the center.
-        this.titleLabelX = (this.getWidth() / 2) - (this.getFont().width(this.getTitle()) / 2);
+        this.titleLabelX = (this.imageWidth - this.font.width(this.getTitle())) / 2;
 
         // Initialize tooltips:
         this.initTooltips();
