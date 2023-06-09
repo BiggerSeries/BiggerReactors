8193631fdcc6b0b434c46e40d67b4a5a049d4dc7
RogueLogix <roguelogix@roguelogix.net>
2023-06-08 17:22:16 -0700

    fix creative tab

--- src/main/java/net/roguelogix/biggerreactors/BiggerReactors.java
+++ src/main/java/net/roguelogix/biggerreactors/BiggerReactors.java
@@ -1,33 +1,32 @@
 package net.roguelogix.biggerreactors;
 
+import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
 import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.roguelogix.biggerreactors.machine.client.CyaniteReprocessorScreen;
 import net.roguelogix.biggerreactors.machine.containers.CyaniteReprocessorContainer;
-import net.roguelogix.biggerreactors.multiblocks.heatexchanger.containers.HeatExchangerFluidPortContainer;
-import net.roguelogix.biggerreactors.multiblocks.heatexchanger.containers.HeatExchangerTerminalContainer;
 import net.roguelogix.biggerreactors.multiblocks.heatexchanger.client.HeatExchangerFluidPortScreen;
 import net.roguelogix.biggerreactors.multiblocks.heatexchanger.client.HeatExchangerTerminalScreen;
+import net.roguelogix.biggerreactors.multiblocks.heatexchanger.containers.HeatExchangerFluidPortContainer;
+import net.roguelogix.biggerreactors.multiblocks.heatexchanger.containers.HeatExchangerTerminalContainer;
 import net.roguelogix.biggerreactors.multiblocks.reactor.client.*;
 import net.roguelogix.biggerreactors.multiblocks.reactor.containers.*;
 import net.roguelogix.biggerreactors.multiblocks.turbine.client.TurbineFluidPortScreen;
 import net.roguelogix.biggerreactors.multiblocks.turbine.client.TurbineTerminalScreen;
 import net.roguelogix.biggerreactors.multiblocks.turbine.containers.TurbineFluidPortContainer;
 import net.roguelogix.biggerreactors.multiblocks.turbine.containers.TurbineTerminalContainer;
-import net.roguelogix.biggerreactors.multiblocks.turbine.tiles.TurbineRotorBearingTile;
 import net.roguelogix.biggerreactors.registries.FluidTransitionRegistry;
 import net.roguelogix.biggerreactors.registries.ReactorModeratorRegistry;
 import net.roguelogix.biggerreactors.registries.TurbineCoilRegistry;
+import net.roguelogix.phosphophyllite.Phosphophyllite;
 import net.roguelogix.phosphophyllite.event.ReloadDataEvent;
 import net.roguelogix.phosphophyllite.registry.Registry;
+import net.roguelogix.quartz.Quartz;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -41,7 +40,7 @@ public class BiggerReactors {
     public static final boolean LOG_DEBUG = LOGGER.isDebugEnabled();
     
     public BiggerReactors() {
-        new Registry();
+        new Registry(new ReferenceArrayList<>(), ReferenceArrayList.of(new ResourceLocation(Phosphophyllite.modid, "creative_tab"), new ResourceLocation(Quartz.modid, "creative_tab")));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientSetup);
         MinecraftForge.EVENT_BUS.addListener(this::onReloadData);
         version = FMLLoader.getLoadingModList().getModFileById(modid).versionString();
